18a3ac1266838650dea04f6a0018c1411c214139
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-12-12 15:13:54 -0500

    Tercer commit: Funcion principal y la llamada da la funcion

--- Farmacia.c
+++ Farmacia.c
@@ -6,16 +6,32 @@
 void Calculo_Insumos_Ingreso(int insumo[30][12], int total[30])
 {
     srand(time(NULL));
-    total[30]= (0);
     for (int i = 0; i < 30; i++)
     {
-        total[i] = total[i] + insumo[i];
+        total[i]=0;
+    }
+    
+    for (int i = 0; i < 30; i++)
+    {
         printf("\nInsumo %d\n", i + 1);
         for (int j = 0; j < 12; j++)
         {
             insumo[i][j] = rand() % 100 + 1;
             printf("%d \t", insumo[i][j]);
         }
+        total[i] = total[i] + insumo[i];
         printf ("Total por producto: %d \n", total[i]);
     }
+}
+
+int main()
+{
+
+    int insumos_ingreso[30][12], insumo_engreso[30], ingreso_total[30], engreso_total[30];
+
+    printf ("Ingresos por año de cada insumo \n");
+    printf("Enero\tFebrero\tMarzo\tAbril\tMayo\tJunio\tAgosto\tSeptiembre\tOctubre\tNoviembre\tDiciembre");
+    Calculo_Insumos_Ingreso(insumos_ingreso,ingreso_total);
+
+    return 0;
 }
