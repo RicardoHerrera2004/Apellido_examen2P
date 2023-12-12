b5b7315d7bdc12aee2c9fe24068087e528c7da5e
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-12-12 15:32:20 -0500

    Cuarto commit: Resolucion del problema

--- Farmacia.c
+++ Farmacia.c
@@ -3,14 +3,10 @@
 #include <time.h>
 // Se incluye la biblioteca principal y las bibliotecas para la creacion de numero random
 
-void Calculo_Insumos_Ingreso(int insumo[30][12], int total[30])
+void Calculo_Insumos_Ingreso(int insumo[30][12])
 {
-    srand(time(NULL));
-    for (int i = 0; i < 30; i++)
-    {
-        total[i]=0;
-    }
-    
+    srand(time(NULL));  
+    int suma=0;  
     for (int i = 0; i < 30; i++)
     {
         printf("\nInsumo %d\n", i + 1);
@@ -18,20 +14,45 @@ void Calculo_Insumos_Ingreso(int insumo[30][12], int total[30])
         {
             insumo[i][j] = rand() % 100 + 1;
             printf("%d \t", insumo[i][j]);
+            suma = suma + insumo[i][j]; 
         }
-        total[i] = total[i] + insumo[i];
-        printf ("Total por producto: %d \n", total[i]);
+        
+        printf ("\nEl total del insumo %d es %d \n", i+1, suma);
+    }
+
+
+    
+}
+
+void Calculo_Insumos_Engreso(int insumo[30][12])
+{
+    srand(time(NULL));
+    int suma=0;
+    for (int i = 0; i < 30; i++)
+    {
+        printf("\nInsumo %d\n", i + 1);
+        for (int j = 0; j < 12; j++)
+        {
+            insumo[i][j] = rand() % 100 + 1;
+            printf("%d \t", insumo[i][j]);
+            suma = suma + insumo[i][j]; 
+        }
+        printf ("\nEl total del insumo %d es %d \n", i+1, suma);
     }
 }
 
 int main()
 {
 
-    int insumos_ingreso[30][12], insumo_engreso[30], ingreso_total[30], engreso_total[30];
+    int insumos_ingreso[30][12], insumo_engreso[30][12];
 
     printf ("Ingresos por año de cada insumo \n");
     printf("Enero\tFebrero\tMarzo\tAbril\tMayo\tJunio\tAgosto\tSeptiembre\tOctubre\tNoviembre\tDiciembre");
-    Calculo_Insumos_Ingreso(insumos_ingreso,ingreso_total);
+    Calculo_Insumos_Ingreso(insumos_ingreso);
+
+    printf ("\nEngresos por año de cada insumo \n");
+    printf("Enero\tFebrero\tMarzo\tAbril\tMayo\tJunio\tAgosto\tSeptiembre\tOctubre\tNoviembre\tDiciembre");
+    Calculo_Insumos_Engreso(insumo_engreso);
 
     return 0;
 }
